130be18a339cddf9f3ffe4859caf73a47ecb4246
soalarii <soaresmachadolarissa@gmail.com>
2023-11-23 19:55:39 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -0,0 +1,19 @@
+package br.com.etecia.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView idRecyclerViewFilmes;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        idRecyclerViewFilmes = findViewById(R.id.idRecyclerViewFilmes);
+    }
+}
