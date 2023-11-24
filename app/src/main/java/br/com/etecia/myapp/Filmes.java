5d6c7319eaef21bfdc791d2aab84ed4fa27df862
soalarii <soaresmachadolarissa@gmail.com>
2023-11-23 21:50:59 -0300

    Quarto commit

--- app/src/main/java/br/com/etecia/myapp/Filmes.java
+++ app/src/main/java/br/com/etecia/myapp/Filmes.java
@@ -11,4 +11,52 @@ public class Filmes {
     private String rating;
 
     private int imgFilmes;
+
+    public Filmes(String titulo, String categoria, String descricao, String rating, int imgFilmes) {
+        this.titulo = titulo;
+        this.categoria = categoria;
+        this.descricao = descricao;
+        this.rating = rating;
+        this.imgFilmes = imgFilmes;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getRating() {
+        return rating;
+    }
+
+    public void setRating(String rating) {
+        this.rating = rating;
+    }
+
+    public int getImgFilmes() {
+        return imgFilmes;
+    }
+
+    public void setImgFilmes(int imgFilmes) {
+        this.imgFilmes = imgFilmes;
+    }
 }
