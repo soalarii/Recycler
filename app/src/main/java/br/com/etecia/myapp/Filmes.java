85c6e86e54bd52a2c4b68df98a195cf8a789b55f
soalarii <soaresmachadolarissa@gmail.com>
2023-11-23 21:48:12 -0300

    Terceiro commit

---
+++ app/src/main/java/br/com/etecia/myapp/Filmes.java
@@ -0,0 +1,14 @@
+package br.com.etecia.myapp;
+
+public class Filmes {
+
+    private String titulo;
+
+    private String categoria;
+
+    private String descricao;
+
+    private String rating;
+
+    private int imgFilmes;
+}
